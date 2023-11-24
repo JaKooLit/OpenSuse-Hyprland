6dc08256c83b767760f67c614c241e083c35ee1d
JaKooLit <jimmielovejay@gmail.com>
2023-11-24 10:29:30 +0900

    updated packman

--- install-scripts/01-packman.sh
+++ install-scripts/01-packman.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+packman_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
 
-############## WARNING DO NOT EDIT BEYOND THIS LINE if you dont know what you are doing! ######################################
 # Determine the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIR="$SCRIPT_DIR/.."
 
 # Change the working directory to the parent directory of the script
-PARENT_DIR="$SCRIPT_DIR/.."
 cd "$PARENT_DIR" || exit 1
 
-# Set some colors for output messages
-OK="$(tput setaf 2)[OK]$(tput sgr0)"
-ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
-NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
-WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
-CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+# Set colors for output messages
+OK=$(tput setaf 2)[OK]$(tput sgr0)
+ERROR=$(tput setaf 1)[ERROR]$(tput sgr0)
+NOTE=$(tput setaf 3)[NOTE]$(tput sgr0)
+WARN=$(tput setaf 166)[WARN]$(tput sgr0)
+CAT=$(tput setaf 6)[ACTION]$(tput sgr0)
 ORANGE=$(tput setaf 166)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
@@ -22,15 +22,12 @@ RESET=$(tput sgr0)
 # Set the name of the log file to include the current date and time
 LOG="install-$(date +%d-%H%M%S)_packman.log"
 
-## adding packman repo and switching over to packman to have all apps available including yad
+# Adding Packman repository and switching over to Packman
+printf "\n%s - Adding Packman repository (Globally).... \n" "${NOTE}"
 
-# Installation of main components
-printf "\n%s - adding packman (Globally).... \n" "${NOTE}"
+sudo zypper -n --quiet ar --refresh -p 90 "$packman_repo" packman 2>&1 | tee -a "$LOG"
+sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
+sudo zypper -n dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
 
-sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman 2>&1 | tee -a "$LOG"
-
-sudo zypper refresh 2>&1 | tee -a "$LOG"
-
-sudo zypper dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
-
+# Clear the terminal after execution
 clear
