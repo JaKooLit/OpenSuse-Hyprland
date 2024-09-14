1c268ed215d77c12aff93f282eeaf6e1cfb9a41e
JaKooLit <ejhay.games@gmail.com>
2024-09-14 13:58:26 +0900

    minor tweak

--- install-scripts/03-opi-pkgs.sh
+++ install-scripts/03-opi-pkgs.sh
@@ -21,7 +21,7 @@ source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 
 
 # Set the name of the log file to include the current date and time
-LOG="Install-Logs/install-$(date +'%d-%H%M%S')_opi.log"
+LOG="Install-Logs/install-$(date +'%d-%H%M%S')_opi-pkgs.log"
 
 # Installing packages from OBS
 printf "${NOTE} Installing packages from OpenSuse Builder Service (OBS)...\n"
