afe0152acf8ea1c6f0df10fb8a84025b079bd1a5
JaKooLit <ejhay.games@gmail.com>
2025-02-06 10:20:25 +0900

    switched back to global packman again

--- install-scripts/00-add-repo.sh
+++ install-scripts/00-add-repo.sh
@@ -3,7 +3,7 @@
 # adding additional repo
 
 #All of Packman
-#packman_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
+packman_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
 
 # Only Essentials
 packman_essentials="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/"
@@ -22,19 +22,28 @@ source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 # Set the name of the log file to include the current date and time
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_add-repo.log"
 
-# Adding Packman repository and switching over to Packman
-#printf "\n%s - Adding ${YELLOW}Packman repository (Globally)${RESET} .... \n" "${NOTE}"
+# Check if Packman repository already in system
+if ! sudo zypper repos | grep -q 'packman'; then
+  printf "\n%s - Adding ${SKY_BLUE}Packman repository (Globally)${RESET} .... \n" "${NOTE}"
 
-#sudo zypper -n --quiet ar --refresh -p 90 "$packman_repo" packman 2>&1 | tee -a "$LOG"
-#sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
-#sudo zypper -n dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
+  sudo zypper -n --quiet ar --refresh -p 90 "$packman_repo" packman 2>&1 | tee -a "$LOG"
+  sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
+  sudo zypper -n dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
+else
+  echo -e "${INFO} ${YELLOW}Packman repository${RESET}  already exists, skipping addition.${RESET}"
+fi
 
-# Adding Packman essential repository
-printf "\n%s - Adding ${YELLOW}Packman repository (Essentials)${RESET} .... \n" "${NOTE}"
 
-sudo zypper -n --quiet ar --refresh -p 90 "$packman_essentials" packman-essentials 2>&1 | tee -a "$LOG"
-sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
-sudo zypper -n dup --from packman-essentials --allow-vendor-change 2>&1 | tee -a "$LOG"
+# Check if Packman-essentials repository already in system
+#if ! sudo zypper repos | grep -q 'packman-essentials'; then
+#  printf "\n%s - Adding ${SKY_BLUE}Packman repository Essentials${RESET} .... \n" "${NOTE}"
+
+#  sudo zypper -n --quiet ar --refresh -p 90 "$packman_essentials" packman-essentials 2>&1 | tee -a "$LOG"
+#  sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
+#  sudo zypper -n dup --from packman-essentials --allow-vendor-change 2>&1 | tee -a "$LOG"
+#else
+#  echo -e "${INFO} ${YELLOW}Packman-essential repository${RESET}  already exists, skipping addition.${RESET}"
+#fi
 
 
 printf "\n%.0s" {1..2}
