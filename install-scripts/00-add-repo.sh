ec627db0dc9b5630dff6a80aa4fcf493cb5698b6
JaKooLit <ejhay.games@gmail.com>
2025-02-06 09:51:54 +0900

    updated packman repo from all to essentials only

--- install-scripts/00-add-repo.sh
+++ install-scripts/00-add-repo.sh
@@ -2,11 +2,13 @@
 # 💫 https://github.com/JaKooLit 💫 #
 # adding additional repo
 
-packman_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
+#All of Packman
+#packman_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
 
-#X11_wayland_repo="https://download.opensuse.org/repositories/X11:Wayland/openSUSE_Tumbleweed/X11:Wayland.repo"
+# Only Essentials
+packman_essentials="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/"
+
                   
-
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
 # Determine the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,14 +23,18 @@ source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_add-repo.log"
 
 # Adding Packman repository and switching over to Packman
-printf "\n%s - Adding ${YELLOW}Packman repository (Globally)${RESET} .... \n" "${NOTE}"
+#printf "\n%s - Adding ${YELLOW}Packman repository (Globally)${RESET} .... \n" "${NOTE}"
 
-sudo zypper -n --quiet ar --refresh -p 80 "$packman_repo" packman 2>&1 | tee -a "$LOG"
+#sudo zypper -n --quiet ar --refresh -p 90 "$packman_repo" packman 2>&1 | tee -a "$LOG"
+#sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
+#sudo zypper -n dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
+
+# Adding Packman essential repository
+printf "\n%s - Adding ${YELLOW}Packman repository (Essentials)${RESET} .... \n" "${NOTE}"
+
+sudo zypper -n --quiet ar --refresh -p 90 "$packman_essentials" packman-essentials 2>&1 | tee -a "$LOG"
 sudo zypper --gpg-auto-import-keys refresh 2>&1 | tee -a "$LOG"
-sudo zypper -n dup --from packman --allow-vendor-change 2>&1 | tee -a "$LOG"
+sudo zypper -n dup --from packman-essentials --allow-vendor-change 2>&1 | tee -a "$LOG"
 
-# disabled for now, its giving too much headache
-# add x11:wayland repo
-#sudo zypper ar --refresh -p 80 "$X11_wayland_repo" X11:Wayland 2>&1 | tee -a "$LOG"
 
 printf "\n%.0s" {1..2}
