8fd6eb0fe692913ad63f10ab64f0cd071ea2388d
JaKooLit <ejhay.games@gmail.com>
2025-03-02 00:18:02 +0900

    updated install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ fi
 # install whiptails if detected not installed. Necessary for this version
 if ! command -v whiptail >/dev/null; then
     echo "${NOTE} - whiptail is not installed. Installing..."
-    sudo zypper in -y --no-recommends whiptail
+    sudo zypper in -y --no-recommends newt
     printf "\n%.0s" {1..1}
 fi
 
